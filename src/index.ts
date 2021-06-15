61847c4b2761e96bb17e7722ab3bbbf8d0c8da3e
Raoul de Heer <raouldeheer@users.noreply.github.com>
2021-06-15 21:51:24 +0200

    Converted js to ts

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import fs from 'fs'
 import JiraApi from 'jira-client'
 import discord from 'discord.js'
 
-const config = JSON.parse(fs.readFileSync('./config.json'))
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'))
 const client = new discord.Client()
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+  
+  console.log(`Logged in as ${client.user!.tag}!`)
   // Set the presence for the bot (Listening to !help)
-  client.user.setPresence({
+
+  client.user!.setPresence({
     status: 'online',
-    game: {
+    activity: {
       name: config.prefix + 'help',
       type: 2
     }
@@ -20,7 +22,7 @@ client.on('ready', () => {
 const jira = new JiraApi({
   protocol: 'https',
   host: config.host,
-  port: 443,
+  port: '443',
   username: config.user,
   password: config.password,
   apiVersion: '2',
