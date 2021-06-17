08d91a4fd6613a5b91780e0c536150f2c6d0d83f
Raoul de Heer <raouldeheer@users.noreply.github.com>
2021-06-17 21:06:14 +0200

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import fs from 'fs'
-import JiraApi from 'jira-client'
-import discord from 'discord.js'
+import fs from 'fs';
+import JiraApi from 'jira-client';
+import discord from 'discord.js';
 import { configOptions } from './types';
 
-const config: configOptions = JSON.parse(fs.readFileSync('./config.json', 'utf-8'))
-const client = new discord.Client()
+const config: configOptions = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+const client = new discord.Client();
 
 client.on('ready', () => {
-  
-  console.log(`Logged in as ${client.user!.tag}!`)
+
+  console.log(`Logged in as ${client.user!.tag}!`);
   // Set the presence for the bot (Listening to !help)
 
   client.user!.setPresence({
     status: 'online',
     activity: {
       name: config.prefix + 'help',
-      type: 2
-    }
-  })
-})
+      type: 2,
+    },
+  });
+});
 
 const jira = new JiraApi({
   protocol: 'https',
@@ -27,12 +27,11 @@ const jira = new JiraApi({
   username: config.user,
   password: config.password,
   apiVersion: '2',
-  strictSSL: true
-})
+  strictSSL: true,
+});
 
-for (const module of ['eigenbot', 'scicraft', 'minecraft-version']) {
-  import('./' + module + '.js').then(m => m.default(client, config, jira))
-}
+['eigenbot', 'scicraft', 'minecraft-version'].forEach(module =>
+  import('./' + module + '.js').then(m => m.default(client, config, jira)));
 
 // Login with token
-client.login(config.token)
+client.login(config.token);
